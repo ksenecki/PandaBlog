526fbc0ae0f47a6d452f79ecd7cc1bf5d7412302
ksenecki <kamil.senecki@deployed.pl>
2021-04-16 13:21:03 +0200

    requirements

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ksenecki/Desktop/projects/ksenecki/blog/PandaBlog/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
